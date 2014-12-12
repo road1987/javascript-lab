85d4341b81defd06cb2662d22c597161846faf1a
road1987 <bee0873@gmail.com>
2014-12-12 11:23:58 +0800

    add stickAuto

---
+++ stickAuto/stickyAuto.js
@@ -0,0 +1,17 @@
+/*
+* PreLoader - jQuery plugin
+* div sticky auto to top
+*
+* Name:			stickyAuto.js
+* Author:		hongchun.li
+* Date:			2014-12-5		
+* Version:		1.0
+* Reference:		
+*	
+*/
+
+(function($) {
+
+	
+
+})(jQuery);
